40cd344d835f3797c0cb0c6faa9384a433a3966c
SamserAliKhan <sasmeralikhan0448@gmail.com>
2023-06-16 01:14:05 +0530

    new file:   URL.txt 	deleted:    modules/URL.txt 	modified:   package-lock.json 	modified:   package.json 	renamed:    modules/qr_iamge.png -> qr_iamge.png 	modified:   server.js 	renamed:    views/index.ejs -> views/index.html

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 const express = require("express");
-const app = express();
-const qr = require('./modules/qr')
+const bodyParser = require('body-parser');
 
-app.set('view engine', 'ejs')
+const app = express();
+
+
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+console.log('pass 1');
 
 app.get('/',function(req,res){
-    res.render('index')
-})
+    console.log('pass 2');
+    res.sendFile(__dirname +"/views"+"/index.html")
+});
 
-app.listen(3000,function(){})
+console.log('pass 3');
+
+var rl;
+
+app.post('/',function(req,res){
+     console.log('pass 4');
+     rl=req.body.Url;
+     res.send('The entred URL is: '+rl);
+     const qr = require('./modules/qr')
+     console.log(rl);
+     
+});
+console.log(rl);
+// app.post('/qr', (req, res) => {
+//     qr.create({ful: req.body.fullUrl})
+//     res.redirect('/')
+// })
+
+app.listen(5000,function(){})
